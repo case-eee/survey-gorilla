5e24e38f2a1d5a686291849bfb3af35d4e73130c
Robb & Jared M Daugherty <pair+ieatkimchi.jmdaugherty@devbootcamp.com>
2014-05-23 17:25:10 -0500

    Update routes and views

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -6,21 +6,50 @@ end
 
 get '/surveys/new' do  # Step1.) Create
 
+  erb :new_survey
 end
 
 post '/surveys' do  # Step2.) Create
+  survey = Survey.create(user_id: current_user.id, title: params[:title])
 
+  redirect '/surveys/#{survey.id}/edit'
 end
 
 get '/surveys/:id' do  # Show specific
+  @survey = Survey.find(params[:id])
+  @questions = @survey.questions
 
+  erb :show_survey
+end
+
+post '/surveys/:id' do
+  @survey = Survey.find(params[:id])
+  Question.create(survey_id: @survey.id, content: params[:content])
+  if params[:complete]
+    redirect "/surveys/#{@survey.id}"
+  elsif params[:add_question]
+    redirect "/surveys/#{@survey.id}/edit"
+  end
 end
 
 get '/surveys/:id/edit' do  # Step1.) Edit
+  @survey = Survey.find(params[:id])
+  @questions = @survey.questions
+
+  erb :add_questions
+end
+
+post '/surveys/:id/questions/:question_id' do
+  @survey = Survey.find(params[:id])
+  @question = @survey.questions.find(params[:question_id])
+
+  @question.update_attributes(params[:question])
+  redirect "/surveys/#{@survey.id}/edit"
 
 end
 
-patch '/surveys/:id' do  # Step2.) Edit
+put '/surveys/:id' do  # Step2.) Edit
+
 
 end
 
