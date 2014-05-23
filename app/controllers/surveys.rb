48550abfb9bc6dc23b0a7554b8bfdffbf2aebc3c
Robb & Jared M Daugherty <pair+ieatkimchi.jmdaugherty@devbootcamp.com>
2014-05-23 13:46:36 -0500

    Finished seeding data

---
+++ app/controllers/surveys.rb
@@ -0,0 +1,27 @@
+get '/surveys' do  # Show all
+
+end
+
+get '/surveys/new' do  # Step1.) Create
+
+end
+
+post '/surveys' do  # Step2.) Create
+
+end
+
+get '/surveys/:id' do  # Show specific
+
+end
+
+get '/surveys/:id/edit' do  # Step1.) Edit
+
+end
+
+patch '/surveys/:id' do  # Step2.) Edit
+
+end
+
+delete '/surveys/:id' do  # Delete
+
+end
