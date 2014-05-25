87b8d5ace4c6f28de093dcc6af97c7023942be7d
Jared M Daugherty <jaredmdaugherty@gmail.com>
2014-05-25 18:40:48 -0500

    Add delete functionality

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -29,7 +29,7 @@ get '/surveys/:id/questions/:counter' do  # Show specific
 
   if request.xhr?
     option = Option.find_by_answer(params[:answer])
-    Response.create(user_id: session[:user_id], option_id: option.id)
+    Response.create(user_id: session[:user_id], option_id: option.id, question_id: @current_question.id)
     { :current_question => @current_question, :current_options => @current_options }.to_json
 
   else
